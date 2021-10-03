b748226dbc6d593cba11675cbe4d2f7f1ddda4d9
Gustavo Freitas <flovatsug@gmail.com>
2021-10-03 20:52:23 -0300

    some another updates

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -21,11 +21,14 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'semi-bold)
 ;;       doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font"))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14)
+      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 14)
+      doom-big-font (font-spec :family "FiraCode Nerd Font" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-wilmersdorf)
+(setq doom-theme 'doom-nord)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
