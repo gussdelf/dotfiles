921e2e79ad229ac88c88327d8bed45a779a1b936
Gustavo Freitas <flovatsug@gmail.com>
2021-11-16 13:08:07 -0300

    Fix: just a minor updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
 # Starship prompt
-# starship init fish | source
+starship init fish | source
 
 # Aliases and abbrs
 alias bat='bat --theme Solarized\ \(dark\)'
@@ -14,9 +14,14 @@ alias lg='lazygit'
 alias nf='neofetch'
 alias g='git'
 alias v='nvim'
+alias e='emacsclient -nw'
+alias ee='emacsclient -nc'
 alias vi='/usr/bin/vim'
 alias se='sudoedit'
+alias cc='clang -o'
+alias pp='clang++ -o'
 alias nw='newsboat'
+#alias cdr='cd (ghq list --full-path | fzf --reverse --height=50%)'
 alias nf='neofetch'
 alias cp='rsync'
 alias uf='ufetch'
@@ -29,11 +34,12 @@ abbr N 'nnn -e'
 
 # Exports and sets
 set --export NNN_FIFO "/tmp/nnn.fifo"
-export VISUAl='nvim' 
+export VISUAl='nvim'
 export EDITOR='nvim'
 export NNN_PLUG='c:fzcd;l:launch;z:autojump;p:preview-tui;P:preview-tabbed;o:fzopen;'
 export PATH="$HOME/go/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/share/gem/ruby/3.0.0/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/Projects/shell:$PATH"
 export PAGER="bat --paging=always"
@@ -45,6 +51,7 @@ source ~/.config/diricons.fish
 xset r rate 210 40
 
 function fish_user_key_bindings
-  bind -s -M insert -m default jk 'commandline -f repaint'
+    bind -s -M insert -m default jk 'commandline -f repaint'
 end
+
 funcsave fish_user_key_bindings
