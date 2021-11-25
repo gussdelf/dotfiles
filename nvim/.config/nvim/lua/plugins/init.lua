f42c0ee6c1e259632741b179e96b6a7ac148ec02
Gustavo Freitas <flovatsug@gmail.com>
2021-11-24 22:33:01 -0300

    Fix: some minors updates and fixes

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,103 +1,103 @@
 -- Packer session
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-require('packer').startup(function()
+require("packer").startup(function()
 	-- The own packer.nvim
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 	-- The themes
-	use{
-		'overcache/NeoSolarized',
+	use({
+		"overcache/NeoSolarized",
 		config = function()
-			require('plugins.configs.themes')
+			require("plugins.configs.themes")
 		end,
-	}
-	use {
-		'folke/tokyonight.nvim',
+	})
+	use({
+		"folke/tokyonight.nvim",
 		config = function()
-			require('plugins.configs.themes')
-		end
-	}
-	use {
-		'marko-cerovac/material.nvim',
-		config = function()
-			require('plugins.configs.themes')
-		end
-	}
-	use{
-		'navarasu/onedark.nvim',
-		config = function()
-			require('plugins.configs.themes')
+			require("plugins.configs.themes")
 		end,
-	}
-	use{
-		'yashguptaz/calvera-dark.nvim',
+	})
+	use({
+		"marko-cerovac/material.nvim",
 		config = function()
-			require('plugins.configs.themes')
+			require("plugins.configs.themes")
 		end,
-	}
+	})
+	use({
+		"navarasu/onedark.nvim",
+		config = function()
+			require("plugins.configs.themes")
+		end,
+	})
+	use({
+		"yashguptaz/calvera-dark.nvim",
+		config = function()
+			require("plugins.configs.themes")
+		end,
+	})
 
 	-- Colorizer
-	use{
-		'norcalli/nvim-colorizer.lua',
+	use({
+		"norcalli/nvim-colorizer.lua",
 		config = function()
-			require('plugins.configs.others')
+			require("plugins.configs.others")
 		end,
-	}
+	})
 
 	-- The statusline
-	use {
-		'hoob3rt/lualine.nvim',
-		requires = {'kyazdani42/nvim-web-devicons', opt = true,},
+	use({
+		"hoob3rt/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 		config = function()
-			require('plugins.configs.statusline')
+			require("plugins.configs.statusline")
 		end,
-	}
+	})
 
 	-- The filemanager
-	use {
-		'kyazdani42/nvim-tree.lua',
+	use({
+		"kyazdani42/nvim-tree.lua",
 		config = function()
-			require('plugins.configs.nvim-tree')
-		end
-	}
+			require("plugins.configs.nvim-tree")
+		end,
+	})
 
 	-- The tree-sitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate',
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
 		config = function()
-			require('plugins.configs.others')
-		end
-	}
-	use 'nvim-treesitter/playground'
+			require("plugins.configs.others")
+		end,
+	})
+	use("nvim-treesitter/playground")
 
 	-- Autoparens
-	use {
-		'windwp/nvim-autopairs',
+	use({
+		"windwp/nvim-autopairs",
 		config = function()
-			require('plugins.configs.others')
-		end
-	}
+			require("plugins.configs.others")
+		end,
+	})
 
 	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = { {'nvim-lua/plenary.nvim'} },
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
 		config = function()
-			require('plugins.configs.telescope')
-		end
-	}
+			require("plugins.configs.telescope")
+		end,
+	})
 
 	-- The LSP stuff
-	use {
-		'neovim/nvim-lspconfig',
+	use({
+		"neovim/nvim-lspconfig",
 		config = function()
-			require('plugins.configs.lspconfig')
-		end
-	}
-	use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-	use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-	use 'L3MON4D3/LuaSnip' -- Snippets plugin
-	use 'glepnir/lspsaga.nvim'
+			require("plugins.configs.lspconfig")
+		end,
+	})
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use("glepnir/lspsaga.nvim")
 end)
