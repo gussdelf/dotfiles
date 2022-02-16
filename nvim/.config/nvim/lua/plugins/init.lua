84721f0dd207c434378dccdedcc2250b2ce9555b
Gustavo Freitas <gussdelf@gmail.com>
2022-02-15 21:05:01 -0300

    lipela

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -12,266 +12,272 @@ require "packer_compiled"
 
 return require("packer").startup {
 	function()
-		use "wbthomason/packer.nvim"
-		use "nvim-lua/plenary.nvim"
-
-		-- Improve startup time
 		use {
-			"lewis6991/impatient.nvim",
-			config = function()
-				require "impatient"
-			end,
-		}
+			"wbthomason/packer.nvim",
+			"nvim-lua/plenary.nvim",
 
-		-- Ui
-		use {
-			-- Theme(s)
+			-- Improve startup time
 			{
-				"EdenEast/nightfox.nvim",
-				event = "VimEnter",
-				requires = { "sainnhe/gruvbox-material" },
+				"lewis6991/impatient.nvim",
 				config = function()
-					require("plugins.configs.others").nightfox()
+					require "impatient"
 				end,
 			},
-			{
-				"kyazdani42/nvim-web-devicons",
-				after = "nightfox.nvim",
-			},
-			{
-				"nvim-lualine/lualine.nvim",
-				opt = true,
-				after = "nightfox.nvim",
-				config = function()
-					require("plugins.configs.others").lualine()
-				end,
-			},
-			--  TODO:: Finish my feline config.
-			-- {
-			-- 	"feline-nvim/feline.nvim",
-			-- 	opt = true,
-			-- 	after = "nightfox.nvim",
-			-- 	requires = { "nvim-lua/lsp-status.nvim" },
-			-- 	config = function()
-			-- 		require "plugins.configs.feline"
-			-- 	end,
-			-- },
-			{
-				"akinsho/bufferline.nvim",
-				opt = true,
-				event = "BufAdd",
-				config = function()
-					require("plugins.configs.others").bufferline()
-				end,
-			},
-		}
 
-		-- Treesitter
-		use {
-			--  PERF:: I think this can get a little bit faster.
+			-- Ui
 			{
-				"nvim-treesitter/nvim-treesitter",
-				-- ft = tsFileTypes,
-				event = "BufRead",
-				config = function()
-					require "plugins.configs.treesitter"
-				end,
-			},
-			{
-				"windwp/nvim-ts-autotag",
-				opt = true,
-				ft = {
-					"html",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-					"typescriptcommon",
-					"vue",
-				},
-				config = function()
-					require("plugins.configs.others").autotags()
-				end,
-			},
-			{
-				"p00f/nvim-ts-rainbow",
-				opt = true,
-				after = "nvim-treesitter",
-				config = function()
-					require("plugins.configs.others").tsrainbow()
-				end,
-			},
-			{
-				"norcalli/nvim-colorizer.lua",
-				ft = lspLangs,
-				config = function()
-					require("plugins.configs.others").colorizer()
-				end,
-			},
-		}
-
-		-- Telescope
-		use {
-			"nvim-telescope/telescope.nvim",
-			module = "telescope",
-			event = "BufWinEnter",
-			requires = {
+				-- Theme(s)
 				{
-					"nvim-telescope/telescope-fzf-native.nvim",
-					run = "make all",
+					"EdenEast/nightfox.nvim",
+					event = "VimEnter",
+					requires = { "sainnhe/gruvbox-material" },
+					config = function()
+						require("plugins.configs.others").nightfox()
+					end,
 				},
-				"nvim-telescope/telescope-file-browser.nvim",
-				"nvim-telescope/telescope-symbols.nvim",
-				"jvgrootveld/telescope-zoxide",
-				-- opt = true,
+				{
+					"kyazdani42/nvim-web-devicons",
+					after = "nightfox.nvim",
+				},
+				{
+					"nvim-lualine/lualine.nvim",
+					opt = true,
+					after = "nightfox.nvim",
+					config = function()
+						require("plugins.configs.others").lualine()
+					end,
+				},
+				--  TODO:: Finish my feline config.
+				-- {
+				-- 	"feline-nvim/feline.nvim",
+				-- 	opt = true,
+				-- 	after = "nightfox.nvim",
+				-- 	requires = { "nvim-lua/lsp-status.nvim" },
+				-- 	config = function()
+				-- 		require "plugins.configs.feline"
+				-- 	end,
+				-- },
+				{
+					"akinsho/bufferline.nvim",
+					opt = true,
+					event = "BufAdd",
+					config = function()
+						require("plugins.configs.others").bufferline()
+					end,
+				},
 			},
-			config = function()
-				require "plugins.configs.telescope"
-			end,
-		}
 
-		-- Some utils plugins
-		use {
+			-- Treesitter
 			{
-				"phaazon/hop.nvim",
+				--  PERF:: I think this can get a little bit faster.
+				{
+					"nvim-treesitter/nvim-treesitter",
+					-- ft = tsFileTypes,
+					event = "BufRead",
+					requires = {
+						"nvim-treesitter/playground",
+						after = "nvim-treesitter",
+					},
+					config = function()
+						require "plugins.configs.treesitter"
+					end,
+				},
+				{
+					"windwp/nvim-ts-autotag",
+					opt = true,
+					ft = {
+						"html",
+						"javascript",
+						"javascriptreact",
+						"typescript",
+						"typescriptreact",
+						"typescriptcommon",
+						"vue",
+					},
+					config = function()
+						require("plugins.configs.others").autotags()
+					end,
+				},
+				{
+					"p00f/nvim-ts-rainbow",
+					opt = true,
+					after = "nvim-treesitter",
+					config = function()
+						require("plugins.configs.others").tsrainbow()
+					end,
+				},
+				{
+					"norcalli/nvim-colorizer.lua",
+					ft = lspLangs,
+					config = function()
+						require("plugins.configs.others").colorizer()
+					end,
+				},
+			},
+
+			-- Telescope
+			{
+				"nvim-telescope/telescope.nvim",
+				module = "telescope",
 				event = "BufWinEnter",
-				config = function()
-					require("plugins.configs.others").hop()
-				end,
-			},
-			{
-				"booperlv/nvim-gomove",
-				event = "BufWinEnter",
-				config = function()
-					require("plugins.configs.others").gomove()
-				end,
-			},
-			{
-				"windwp/nvim-autopairs",
-				after = "nvim-cmp",
-				config = function()
-					require("plugins.configs.others").autopairs()
-				end,
-			},
-			{
-				"numToStr/Comment.nvim",
-				-- opt = true,
-				config = function()
-					require("Comment").setup()
-				end,
-			},
-		}
-
-		-- Snippets
-		use {
-			{
-				"rafamadriz/friendly-snippets",
-				evert = "InsertCharPre",
-			},
-			{
-				"L3MON4D3/LuaSnip",
-				after = "nvim-cmp",
-				wants = "friendly-snippets",
-				config = function()
-					require("plugins.configs.others").luasnip()
-				end,
-			},
-		}
-
-		-- Cmp
-		use {
-			{
-				"hrsh7th/nvim-cmp",
-				ft = lspLangs,
-				config = function()
-					require "plugins.configs.cmp"
-				end,
-			},
-			{
-				"saadparwaiz1/cmp_luasnip",
-				after = "LuaSnip",
-			},
-			{
-				"hrsh7th/cmp-nvim-lua",
-				after = "cmp_luasnip",
-			},
-			{
-				"hrsh7th/cmp-nvim-lsp",
-				after = "cmp-nvim-lua",
-			},
-			{
-				"hrsh7th/cmp-buffer",
-				after = "cmp-nvim-lsp",
-			},
-			{
-				"hrsh7th/cmp-path",
-				after = "cmp-buffer",
-			},
-			{
-				"andersevenrud/cmp-tmux",
-				after = "cmp-buffer",
-			},
-		}
-
-		-- Lsp stuff
-		use {
-			{
-				"neovim/nvim-lspconfig",
-				ft = lspLangs,
-				config = function()
-					require "plugins.configs.lspconfig"
-				end,
-			},
-			{
-				"ray-x/lsp_signature.nvim",
-				ft = lspLangs,
-				config = function()
-					require("plugins.configs.others").signature()
-				end,
-			},
-			{
-				"ray-x/navigator.lua",
-				ft = lspLangs,
 				requires = {
-					"ray-x/guihua.lua",
-					run = "cd lua/fzy && make",
+					{
+						"nvim-telescope/telescope-fzf-native.nvim",
+						run = "make all",
+					},
+					"nvim-telescope/telescope-file-browser.nvim",
+					"nvim-telescope/telescope-symbols.nvim",
+					"jvgrootveld/telescope-zoxide",
+					-- opt = true,
 				},
 				config = function()
-					require("plugins.configs.others").navigator()
+					require "plugins.configs.telescope"
 				end,
 			},
-			{
-				"folke/trouble.nvim",
-				ft = lspLangs,
-				-- after = "nvim-lspconfig",
-				config = function()
-					require("plugins.configs.others").trouble()
-				end,
-			},
-			{
-				"jose-elias-alvarez/null-ls.nvim",
-				ft = lspLangs,
-				config = function()
-					require("plugins.configs.others").null_ls()
-				end,
-			},
-		}
 
-		-- Etc
-		use {
+			-- Some utils plugins
 			{
-				"akinsho/toggleterm.nvim",
-				opt = true,
-				event = "BufWinEnter",
-				config = function()
-					require "plugins.configs.term"
-				end,
+				{
+					"phaazon/hop.nvim",
+					event = "BufWinEnter",
+					config = function()
+						require("plugins.configs.others").hop()
+					end,
+				},
+				{
+					"booperlv/nvim-gomove",
+					event = "BufWinEnter",
+					config = function()
+						require("plugins.configs.others").gomove()
+					end,
+				},
+				{
+					"windwp/nvim-autopairs",
+					after = "nvim-cmp",
+					config = function()
+						require("plugins.configs.others").autopairs()
+					end,
+				},
+				{
+					"numToStr/Comment.nvim",
+					-- opt = true,
+					config = function()
+						require("Comment").setup()
+					end,
+				},
 			},
+
+			-- Snippets
 			{
-				"iamcco/markdown-preview.nvim",
-				opt = true,
-				ft = { "markdown" },
-				run = "cd app && yarn install",
+				{
+					"rafamadriz/friendly-snippets",
+					evert = "InsertCharPre",
+				},
+				{
+					"L3MON4D3/LuaSnip",
+					after = "nvim-cmp",
+					wants = "friendly-snippets",
+					config = function()
+						require("plugins.configs.others").luasnip()
+					end,
+				},
+			},
+
+			-- Cmp
+			{
+				{
+					"hrsh7th/nvim-cmp",
+					ft = lspLangs,
+					config = function()
+						require "plugins.configs.cmp"
+					end,
+				},
+				{
+					"saadparwaiz1/cmp_luasnip",
+					after = "LuaSnip",
+				},
+				{
+					"hrsh7th/cmp-nvim-lua",
+					after = "cmp_luasnip",
+				},
+				{
+					"hrsh7th/cmp-nvim-lsp",
+					after = "cmp-nvim-lua",
+				},
+				{
+					"hrsh7th/cmp-buffer",
+					after = "cmp-nvim-lsp",
+				},
+				{
+					"hrsh7th/cmp-path",
+					after = "cmp-buffer",
+				},
+				{
+					"andersevenrud/cmp-tmux",
+					after = "cmp-buffer",
+				},
+			},
+
+			-- Lsp stuff
+			{
+				{
+					"neovim/nvim-lspconfig",
+					ft = lspLangs,
+					config = function()
+						require "plugins.configs.lspconfig"
+					end,
+				},
+				{
+					"ray-x/lsp_signature.nvim",
+					ft = lspLangs,
+					config = function()
+						require("plugins.configs.others").signature()
+					end,
+				},
+				{
+					"ray-x/navigator.lua",
+					ft = lspLangs,
+					requires = {
+						"ray-x/guihua.lua",
+						run = "cd lua/fzy && make",
+					},
+					config = function()
+						require("plugins.configs.others").navigator()
+					end,
+				},
+				{
+					"folke/trouble.nvim",
+					ft = lspLangs,
+					-- after = "nvim-lspconfig",
+					config = function()
+						require("plugins.configs.others").trouble()
+					end,
+				},
+				{
+					"jose-elias-alvarez/null-ls.nvim",
+					ft = lspLangs,
+					config = function()
+						require("plugins.configs.others").null_ls()
+					end,
+				},
+			},
+
+			-- Etc
+			{
+				{
+					"akinsho/toggleterm.nvim",
+					opt = true,
+					event = "BufWinEnter",
+					config = function()
+						require "plugins.configs.term"
+					end,
+				},
+				{
+					"iamcco/markdown-preview.nvim",
+					opt = true,
+					ft = { "markdown" },
+					run = "cd app && yarn install",
+				},
 			},
 		}
 	end,
