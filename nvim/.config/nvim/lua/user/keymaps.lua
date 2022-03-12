2c8e484d5d2735bf706d34e28aa08cf4ea5f83c6
Gustavo Freitas <gussdelf@gmail.com>
2022-03-12 19:46:23 -0300

    add: window keymaps neovim

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -4,16 +4,33 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Window managment
-vim.keymap.set("n", "<leader>q", "<cmd>bw<cr>", { silent = true }) -- Close buffer
+
 vim.keymap.set("n", "<leader>wj", "<cmd>wincmd j<cr>", { silent = true }) -- Window down
 vim.keymap.set("n", "<leader>wk", "<cmd>wincmd k<cr>", { silent = true }) -- Window up
 vim.keymap.set("n", "<leader>wl", "<cmd>wincmd l<cr>", { silent = true }) -- Window right
 vim.keymap.set("n", "<leader>wh", "<cmd>wincmd h<cr>", { silent = true }) -- Window left
-vim.keymap.set("n", "<leader>ww", "<cmd>vsplit<cr> <C-w>l", { silent = true }) -- Vertical split window
+vim.keymap.set("n", "<leader>ww", "<cmd>wincmd w<cr>", { silent = true }) -- Window alternatve
+
+vim.keymap.set("n", "<leader>wJ", "<cmd>wincmd J<cr>", { silent = true }) -- Move window down
+vim.keymap.set("n", "<leader>wK", "<cmd>wincmd K<cr>", { silent = true }) -- Move window up
+vim.keymap.set("n", "<leader>wL", "<cmd>wincmd L<cr>", { silent = true }) -- Move window right
+vim.keymap.set("n", "<leader>wH", "<cmd>wincmd H<cr>", { silent = true }) -- Move window left
+
+vim.keymap.set("n", "<leader>wt", "<cmd>wincmd T<cr>", { silent = true }) -- Move window to a new tab
+vim.keymap.set("n", "<leader>wx", "<cmd>wincmd x<cr>", { silent = true }) -- Swap window
+
+vim.keymap.set("n", "<leader>w=", "<cmd>wincmd =<cr>", { silent = true }) -- Make window equally
+vim.keymap.set("n", "<leader>w+", "<cmd>wincmd +<cr>", { silent = true }) -- Increase current window
+vim.keymap.set("n", "<leader>w-", "<cmd>wincmd -<cr>", { silent = true }) -- Decrease current window
+
+vim.keymap.set("n", "<leader>wv", "<cmd>vsplit | wincmd h<cr>", { silent = true }) -- Vertical split window
+vim.keymap.set("n", "<leader>ws", "<cmd>split | wincmd j<cr>", { silent = true }) -- Horizontal split window
+vim.keymap.set("n", "<leader>wq", "<cmd>close<cr>", { silent = true }) -- Close split window
 
 -- Buffer
 vim.keymap.set("n", "<leader>bj", "<cmd>bnext<CR>", { silent = true })
 vim.keymap.set("n", "<leader>bk", "<cmd>bprevious<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wq", "<cmd>bw<cr>", { silent = true }) -- Close buffer
 
 -- Moviment
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
